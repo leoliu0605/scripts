95d1f020eb18fe04e1a3059cd01ceda4c6f7fecd
Leo <jafee201153@gmail.com>
2025-02-21 17:44:56 +0800

    feat(opencommit.sh): add HCP Vault integration for secure API key management

--- opencommit/opencommit.sh
+++ opencommit/opencommit.sh
@@ -15,12 +15,37 @@ if ! command -v oco >/dev/null 2>&1; then
     npm install -g opencommit
 fi
 
-oco config set OCO_AI_PROVIDER=openai # or deepseek ... wait for the next release
-oco config set OCO_API_KEY=sk-tswNquAPTcW9odiy319FLQ
-oco config set OCO_API_URL=https://chatapi.akash.network/api/v1
-oco config set OCO_MODEL=nvidia-Llama-3-1-Nemotron-70B-Instruct-HF # or DeepSeek-R1-Distill-Qwen-14B ... wait for the next release
+if ! command -v hcp >/dev/null 2>&1; then
+    if [[ "$(uname)" == "Darwin" ]]; then
+        echo "HCP not found, installing on macOS..."
+        if ! command -v brew >/dev/null 2>&1; then
+            echo "Homebrew is not installed. Please install Homebrew."
+            exit 1
+        fi
+        brew tap hashicorp/tap
+        brew install hashicorp/tap/hcp
+    elif [[ "$(uname)" == "Linux" ]]; then
+        echo "HCP not found, installing on Ubuntu..."
+        sudo apt-get update &&
+            sudo apt-get install wget gpg coreutils -y
+        wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+        sudo apt-get update && sudo apt-get install hcp -y
+    else
+        echo "Unsupported OS for HCP installation."
+        exit 1
+    fi
+fi
+
+hcp auth login
+hcp profile init --vault-secrets
+
+oco config set OCO_AI_PROVIDER=gemini
+oco config set OCO_API_KEY=$(hcp vault-secrets secrets open Gemini | awk -F ': ' '/Value/ {print $2}' | xargs)
+oco config set OCO_API_URL=
+oco config set OCO_MODEL=gemini-2.0-flash
 oco config set OCO_ONE_LINE_COMMIT=true
-oco config set OCO_WHY=true
+oco config set OCO_WHY=false
 oco config set OCO_GITPUSH=false
 
 # TODO: https://github.com/di-sukharev/opencommit?tab=readme-ov-file#switch-to-commitlint
